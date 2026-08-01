be783cf6cebfe89e06e7e1afef1ff99441465c47
LuizZuko <Luizhenriquezukovskimangini@gmail.com>
2026-07-31 23:00:47 -0300

    racas de paraguaios com foreach no php

---
+++ Atividade09.php
@@ -0,0 +1,12 @@
+<?php
+$racasPapagaio = [
+    "Papagaio-pirata",
+    "Papagaio-Azul",
+    "Papagaio-Vermelho(PT)"
+];
+
+$racasPapagaio[] = "Papagaio-campeiro";
+
+foreach ($racasPapagaio as $raca) {
+    echo $raca . "  /  ";
+}
