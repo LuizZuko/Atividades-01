1fbaa95a5a6bbabfb3e7c16bd31b7c8c8503526f
LuizZuko <Luizhenriquezukovskimangini@gmail.com>
2026-07-31 22:55:47 -0300

    memes top do professor com alex de esterr egg

---
+++ Atividade08.php
@@ -0,0 +1,12 @@
+<?php
+$memes = [
+    "Cachorro cururu",
+    "Seco seco(1155 do ET)",
+    "Ai chaves",
+    "Alex e sua cor favorita",
+    "Filho do piseiro"
+];
+
+foreach ($memes as $meme) {
+    echo $meme . "  /  ";
+}
